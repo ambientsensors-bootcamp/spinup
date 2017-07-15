00b408f7351ce6d443536d7321d55ca4d5781575
Sharihan ali <sharihanali@u.boisestate.edu>
2017-07-15 15:43:42 -0600

    finished creating my app

--- ng-sharihan-app/src/app/app.component.ts
+++ ng-sharihan-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+ 
+ @Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+ })
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+ export class AppComponent {
+   title = 'Sharihan\'s Website';
+   show = false;
+   buttonTitle = "Show"
+ 
+ fun(){
+   this.show = !this.show;
+   if(this.show){
+     this.buttonTitle = "Hide";
+   }else{
+     this.buttonTitle = "Show";
+   }
+ }
 }
